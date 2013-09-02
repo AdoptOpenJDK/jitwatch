639d71a480bbf0b44e7561357f0015ac8781b85c
Chris Newland <chris@chrisnewland.com>
2013-09-02 19:07:31 +0100

    instructions for setting $JDK_HOME

--- launchHeadless.sh
+++ launchHeadless.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
+
+#uncomment the next line and set JDK_HOME if required
+#export JDK_HOME= 
+
 export CP=bin
 $JDK_HOME/bin/java -cp $CP com.chrisnewland.jitwatch.launch.LaunchHeadless $1 $2
--- launchUI.sh
+++ launchUI.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
+
+#uncomment the next line and set JDK_HOME if required
+#export JDK_HOME=
+
 export CP=$JDK_HOME/lib/tools.jar:$JDK_HOME/jre/lib/jfxrt.jar:bin
 $JDK_HOME/bin/java -cp $CP com.chrisnewland.jitwatch.launch.LaunchUI
