91586359da8f1c86419b74390c2c514b3f036c7a
Martijn Verburg <martijnverburg@gmail.com>
2013-09-06 13:05:13 +0100

    fix POM error message about licencse

--- launchUI.sh
+++ launchUI.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #uncomment the next line and set JDK_HOME if required
-#export JDK_HOME=
+export JDK_HOME=/Library/Java/JavaVirtualMachines//jdk1.7.0_25.jdk/Contents/Home
 
 export CP=$JDK_HOME/lib/tools.jar:$JDK_HOME/jre/lib/jfxrt.jar:bin
 $JDK_HOME/bin/java -cp $CP com.chrisnewland.jitwatch.launch.LaunchUI
