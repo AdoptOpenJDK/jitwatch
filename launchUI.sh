ec2eb320380276fe93cfa39e344a967f783e9d37
Chris Newland <chris@chrisnewland.com>
2013-09-13 11:10:27 +0100

    ant jar, mvn exec:java now working

--- launchUI.sh
+++ launchUI.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-#uncomment the next line and set JDK_HOME if required
-export JDK_HOME=/Library/Java/JavaVirtualMachines//jdk1.7.0_25.jdk/Contents/Home
+#uncomment the next line and set JDK_HOME as required
+#export JDK_HOME=/Library/Java/JavaVirtualMachines/jdk1.7.0_25.jdk/Contents/Home
 
-export CP=$JDK_HOME/lib/tools.jar:$JDK_HOME/jre/lib/jfxrt.jar:bin
+export CP=$JDK_HOME/lib/tools.jar:$JDK_HOME/jre/lib/jfxrt.jar:target/jitwatch-1.0.0-SNAPSHOT.jar
 $JDK_HOME/bin/java -cp $CP com.chrisnewland.jitwatch.launch.LaunchUI
