08fd9416b95cc75544a1b7b916075d388daf950d
Chris Newland <chrisn@allipo.com>
2013-10-12 17:12:54 +0100

    Support hiding classes with no JIT-compiled members from class tree

--- src/main/java/com/chrisnewland/jitwatch/core/JITWatchConfig.java
+++ src/main/java/com/chrisnewland/jitwatch/core/JITWatchConfig.java
@@ -19,150 +19,166 @@ import com.chrisnewland.jitwatch.util.StringUtil;
 
 public class JITWatchConfig
 {
-	private static final String PROPERTIES_FILENAME = "jitwatch.properties";
+    private static final String PROPERTIES_FILENAME = "jitwatch.properties";
 
-	private static final String KEY_ALLOWED_PACKAGES = "PackageFilter";
-	private static final String KEY_SOURCE_LOCATIONS = "Sources";
-	private static final String KEY_CLASS_LOCATIONS = "Classes";
-	private static final String KEY_SHOW_JIT_ONLY = "JitOnly";
-	private static final String KEY_SHOW_HIDE_INTERFACES = "HideInterfaces";
+    private static final String KEY_ALLOWED_PACKAGES = "PackageFilter";
+    private static final String KEY_SOURCE_LOCATIONS = "Sources";
+    private static final String KEY_CLASS_LOCATIONS = "Classes";
+    private static final String KEY_SHOW_JIT_ONLY_MEMBERS = "JitOnly";
+    private static final String KEY_SHOW_JIT_ONLY_CLASSES = "JitOnlyClasses";
+    private static final String KEY_SHOW_HIDE_INTERFACES = "HideInterfaces";
 
-	private IJITListener logListener;
+    private IJITListener logListener;
 
-	private List<String> allowedPackages = new ArrayList<>();
-	private List<String> sourceLocations = new ArrayList<>();
-	private List<String> classLocations = new ArrayList<>();
-	
-	private boolean showOnlyCompiled = true;
+    private List<String> allowedPackages = new ArrayList<>();
+    private List<String> sourceLocations = new ArrayList<>();
+    private List<String> classLocations = new ArrayList<>();
+
+    private boolean showOnlyCompiledMembers = true;
+    private boolean showOnlyCompiledClasses = false;
+
     private boolean hideInterfaces = true;
 
-	public JITWatchConfig(IJITListener logListener)
-	{
-		this.logListener = logListener;
-		loadConfig();
-	}
-
-	private void loadConfig()
-	{
-		Properties loadProps = new Properties();
-
-		try (FileReader fr = new FileReader(getConfigFile()))
-		{
-			loadProps.load(fr);
-		}
-		catch (FileNotFoundException fnf)
-		{
-
-		}
-		catch (IOException ioe)
-		{
-			logListener.handleErrorEntry(ioe.toString());
-		}
-
-		String confPackages = loadProps.getProperty(KEY_ALLOWED_PACKAGES);
-		String confClasses = loadProps.getProperty(KEY_CLASS_LOCATIONS);
-		String confSources = loadProps.getProperty(KEY_SOURCE_LOCATIONS);
-
-		if (confPackages != null && confPackages.trim().length() > 0)
-		{
-			allowedPackages = StringUtil.textToList(confPackages, ",");
-		}
-
-		if (confClasses != null && confClasses.trim().length() > 0)
-		{
-			classLocations = StringUtil.textToList(confClasses, ",");
-		}
-
-		if (confSources != null && confSources.trim().length() > 0)
-		{
-			sourceLocations = StringUtil.textToList(confSources, ",");
-		}
-		
-		showOnlyCompiled = Boolean.parseBoolean(loadProps.getProperty(KEY_SHOW_JIT_ONLY, Boolean.TRUE.toString()));
-		hideInterfaces = Boolean.parseBoolean(loadProps.getProperty(KEY_SHOW_HIDE_INTERFACES, Boolean.TRUE.toString()));
-	}
-
-	public void saveConfig()
-	{
-		Properties saveProps = new Properties();
-
-		saveProps.put(KEY_ALLOWED_PACKAGES, StringUtil.listToText(allowedPackages, ","));
-		saveProps.put(KEY_SOURCE_LOCATIONS, StringUtil.listToText(sourceLocations, ","));
-		saveProps.put(KEY_CLASS_LOCATIONS, StringUtil.listToText(classLocations, ","));
-        saveProps.put(KEY_SHOW_JIT_ONLY, Boolean.toString(showOnlyCompiled));
-        saveProps.put(KEY_SHOW_HIDE_INTERFACES, Boolean.toString(hideInterfaces));
-
-		try (FileWriter fw = new FileWriter(getConfigFile()))
-		{
-			saveProps.store(fw, null);
-		}
-		catch (IOException ioe)
-		{
-			logListener.handleErrorEntry(ioe.toString());
-		}
-	}
-
-	private File getConfigFile()
-	{
-		return new File(System.getProperty("user.dir"), PROPERTIES_FILENAME);
-	}
-
-	public boolean isAllowedPackage(String fqMethodName)
-	{
-		return allowedPackages.size() == 0 || checkPackage(fqMethodName);
-	}
-
-	private boolean checkPackage(String packageName)
-	{
-		for (String allowedPackage : allowedPackages)
-		{
-			if (allowedPackage.equals(packageName) || packageName.startsWith(allowedPackage + "."))
-			{
-				return true;
-			}
-		}
-
-		return false;
-	}
-
-	public List<String> getAllowedPackages()
-	{
-		return Collections.unmodifiableList(allowedPackages);
-	}
-
-	public List<String> getClassLocations()
-	{
-		return Collections.unmodifiableList(classLocations);
-	}
-
-	public List<String> getSourceLocations()
-	{
-		return Collections.unmodifiableList(sourceLocations);
-	}
-
-	public void setAllowedPackages(List<String> allowedPackages)
-	{
-		this.allowedPackages = allowedPackages;
-	}
-
-	public void setSourceLocations(List<String> sourceLocations)
-	{
-		this.sourceLocations = sourceLocations;
-	}
-
-	public void setClassLocations(List<String> classLocations)
-	{
-		this.classLocations = classLocations;
-	}
-	
-    public boolean isShowOnlyCompiled()
+    public JITWatchConfig(IJITListener logListener)
     {
-        return showOnlyCompiled;
+        this.logListener = logListener;
+        loadConfig();
     }
 
-    public void setShowOnlyCompiled(boolean showOnlyCompiled)
+    private void loadConfig()
     {
-        this.showOnlyCompiled = showOnlyCompiled;
+        Properties loadProps = new Properties();
+
+        try (FileReader fr = new FileReader(getConfigFile()))
+        {
+            loadProps.load(fr);
+        }
+        catch (FileNotFoundException fnf)
+        {
+
+        }
+        catch (IOException ioe)
+        {
+            logListener.handleErrorEntry(ioe.toString());
+        }
+
+        String confPackages = loadProps.getProperty(KEY_ALLOWED_PACKAGES);
+        String confClasses = loadProps.getProperty(KEY_CLASS_LOCATIONS);
+        String confSources = loadProps.getProperty(KEY_SOURCE_LOCATIONS);
+
+        if (confPackages != null && confPackages.trim().length() > 0)
+        {
+            allowedPackages = StringUtil.textToList(confPackages, ",");
+        }
+
+        if (confClasses != null && confClasses.trim().length() > 0)
+        {
+            classLocations = StringUtil.textToList(confClasses, ",");
+        }
+
+        if (confSources != null && confSources.trim().length() > 0)
+        {
+            sourceLocations = StringUtil.textToList(confSources, ",");
+        }
+
+        showOnlyCompiledMembers = Boolean.parseBoolean(loadProps.getProperty(KEY_SHOW_JIT_ONLY_MEMBERS, Boolean.TRUE.toString()));
+        showOnlyCompiledClasses = Boolean.parseBoolean(loadProps.getProperty(KEY_SHOW_JIT_ONLY_CLASSES, Boolean.FALSE.toString()));
+
+        hideInterfaces = Boolean.parseBoolean(loadProps.getProperty(KEY_SHOW_HIDE_INTERFACES, Boolean.TRUE.toString()));
+    }
+
+    public void saveConfig()
+    {
+        Properties saveProps = new Properties();
+
+        saveProps.put(KEY_ALLOWED_PACKAGES, StringUtil.listToText(allowedPackages, ","));
+        saveProps.put(KEY_SOURCE_LOCATIONS, StringUtil.listToText(sourceLocations, ","));
+        saveProps.put(KEY_CLASS_LOCATIONS, StringUtil.listToText(classLocations, ","));
+        saveProps.put(KEY_SHOW_JIT_ONLY_MEMBERS, Boolean.toString(showOnlyCompiledMembers));
+        saveProps.put(KEY_SHOW_JIT_ONLY_CLASSES, Boolean.toString(showOnlyCompiledClasses));
+        saveProps.put(KEY_SHOW_HIDE_INTERFACES, Boolean.toString(hideInterfaces));
+
+        try (FileWriter fw = new FileWriter(getConfigFile()))
+        {
+            saveProps.store(fw, null);
+        }
+        catch (IOException ioe)
+        {
+            logListener.handleErrorEntry(ioe.toString());
+        }
+    }
+
+    private File getConfigFile()
+    {
+        return new File(System.getProperty("user.dir"), PROPERTIES_FILENAME);
+    }
+
+    public boolean isAllowedPackage(String fqMethodName)
+    {
+        return allowedPackages.size() == 0 || checkPackage(fqMethodName);
+    }
+
+    private boolean checkPackage(String packageName)
+    {
+        for (String allowedPackage : allowedPackages)
+        {
+            if (allowedPackage.equals(packageName) || packageName.startsWith(allowedPackage + "."))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public List<String> getAllowedPackages()
+    {
+        return Collections.unmodifiableList(allowedPackages);
+    }
+
+    public List<String> getClassLocations()
+    {
+        return Collections.unmodifiableList(classLocations);
+    }
+
+    public List<String> getSourceLocations()
+    {
+        return Collections.unmodifiableList(sourceLocations);
+    }
+
+    public void setAllowedPackages(List<String> allowedPackages)
+    {
+        this.allowedPackages = allowedPackages;
+    }
+
+    public void setSourceLocations(List<String> sourceLocations)
+    {
+        this.sourceLocations = sourceLocations;
+    }
+
+    public void setClassLocations(List<String> classLocations)
+    {
+        this.classLocations = classLocations;
+    }
+
+    public boolean isShowOnlyCompiledMembers()
+    {
+        return showOnlyCompiledMembers;
+    }
+
+    public void setShowOnlyCompiledMembers(boolean showOnlyCompiledMembers)
+    {
+        this.showOnlyCompiledMembers = showOnlyCompiledMembers;
+    }
+    
+    public boolean isShowOnlyCompiledClasses()
+    {
+        return showOnlyCompiledClasses;
+    }
+
+    public void setShowOnlyCompiledClasses(boolean showOnlyCompiledClasses)
+    {
+        this.showOnlyCompiledClasses = showOnlyCompiledClasses;
     }
 
     public boolean isHideInterfaces()
