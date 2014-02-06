d11c57b92395817bdf147f3faed0f30c258cb66c
Chris Newland <chris@chrisnewland.com>
2014-02-06 12:22:44 +0000

    old style test for when shell is not bash

--- launchUI.sh
+++ launchUI.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 unamestr=`uname`
-if [[ "$unamestr" == 'Darwin' ]]; then
+if [ "$unamestr" = 'Darwin' ]; then
    export JDK_HOME=`/usr/libexec/java_home`
 fi
 
 export CP=$JDK_HOME/lib/tools.jar:$JDK_HOME/jre/lib/jfxrt.jar:target/jitwatch-1.0.0-SNAPSHOT.jar
-$JDK_HOME/bin/java -cp $CP com.chrisnewland.jitwatch.launch.LaunchUI
+$JDK_HOME/bin/java -Xms500m -Xmx1500m -cp $CP com.chrisnewland.jitwatch.launch.LaunchUI
