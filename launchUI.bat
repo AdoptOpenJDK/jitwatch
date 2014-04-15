01f681533c90adb9b80832ffcbc5bb67183a3ce4
Chris Newland <chris@chrisnewland.com>
2014-04-15 09:35:54 +0100

    ant refactors

--- launchUI.bat
+++ launchUI.bat
@@ -6,8 +6,15 @@
 @ECHO OFF
 
 @REM ---------------------------------------------------------------
-     @set CP="%JAVA_HOME%\lib\tools.jar;%JAVA_HOME%\jre\lib\jfxrt.jar;target\jitwatch-1.0.0-SNAPSHOT.jar" 
-     java -classpath "%JAVA_HOME%\lib\tools.jar;%JAVA_HOME%\jre\lib\jfxrt.jar;target\jitwatch-1.0.0-SNAPSHOT.jar" com.chrisnewland.jitwatch.launch.LaunchUI
-@REM ---------------------------------------------------------------
 
+@set CLASSPATH="%CLASSPATH%;lib\logback-classic-1.0.1.jar"
+@set CLASSPATH="%CLASSPATH%;lib\logback-core-1.0.1.jar"
+@set CLASSPATH="%CLASSPATH%;lib\slf4j-api-1.6.4.jar"
+@set CLASSPATH="%CLASSPATH%;%JAVA__HOME%\lib\tools.jar"
+@set CLASSPATH="%CLASSPATH%;%JAVA__HOME%\jre\lib\jfxrt.jar"
+@set CLASSPATH="%CLASSPATH%;target\jitwatch-1.0.0-SNAPSHOT.jar"
+
+
+     java -classpath "%CLASSPATH%" com.chrisnewland.jitwatch.launch.LaunchUI
+@REM ---------------------------------------------------------------
 
