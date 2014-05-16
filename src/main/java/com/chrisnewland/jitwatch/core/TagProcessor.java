75feef0c7a5cefb037214e6fd564c5880a726a2b
neomatrix369 <sadhak001@gmail.com>
2014-05-16 22:34:46 +0100

    Reformatting open and closed braces.

--- src/main/java/com/chrisnewland/jitwatch/core/TagProcessor.java
+++ src/main/java/com/chrisnewland/jitwatch/core/TagProcessor.java
@@ -165,8 +165,8 @@ public class TagProcessor
 		    (name.equals(currentTag.getName())) &&
 		    (currentTag.getParent() == null))
         {
-            result = currentTag;
-		}
+                result = currentTag;
+        }
 		else
 		{
 			// not closed
