7231da878828099576512ec13e68e56fb867c185
Chris Newland <chris@chrisnewland.com>
2014-11-02 23:26:33 +0000

    Accepts parameter for jar to scan or defaults to $JAVA_HOME rt.jar

--- jarScan.sh
+++ jarScan.sh
@@ -10,7 +10,11 @@ else
   fi
 fi
 
+if [ $# -lt 1 ]; then
 export TARGET_JAR=$JAVA_HOME/jre/lib/rt.jar 
+else
+export TARGET_JAR=$1
+fi
 
 echo "Scanning $TARGET_JAR for methods above the default inlining threshold"
 
