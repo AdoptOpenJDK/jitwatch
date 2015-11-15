644ca16474f0c148d68f2b3827f9ac5981839d70
Chris Newland <chris@chrisnewland.com>
2015-11-15 21:16:01 +0000

    Updated Copyright headers

--- src/main/java/org/adoptopenjdk/jitwatch/model/assembly/AssemblyLabels.java
+++ src/main/java/org/adoptopenjdk/jitwatch/model/assembly/AssemblyLabels.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013, 2014 Chris Newland.
+ * Copyright (c) 2013-2015 Chris Newland.
  * Copyright (c) 2015 Philip Aston
  * Licensed under https://github.com/AdoptOpenJDK/jitwatch/blob/master/LICENSE-BSD
  * Instructions: https://github.com/AdoptOpenJDK/jitwatch/wiki
--- src/main/java/org/adoptopenjdk/jitwatch/util/HeadlessUtil.java
+++ src/main/java/org/adoptopenjdk/jitwatch/util/HeadlessUtil.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2013-2015 Chris Newland.
+ * Licensed under https://github.com/AdoptOpenJDK/jitwatch/blob/master/LICENSE-BSD
+ * Instructions: https://github.com/AdoptOpenJDK/jitwatch/wiki
+ */
 package org.adoptopenjdk.jitwatch.util;
 
 import static org.adoptopenjdk.jitwatch.core.JITWatchConstants.ATTR_BYTES;
