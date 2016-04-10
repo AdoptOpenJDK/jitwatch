54de91c22727bc286ab8e4ed25f876b9e916b801
Chris Newland <chris@chrisnewland.com>
2016-04-10 21:16:30 +0100

    Histo Plotter start scripts

---
+++ histoPlotter.bat
@@ -0,0 +1,21 @@
+@REM ---------------------------------------------------------------
+@REM JITWatch
+@REM ---------------------------------------------------------------
+
+@REM Startup script for JITWatch on Windows
+@ECHO OFF
+
+@REM ---------------------------------------------------------------
+
+set CLASSPATH=lib\logback-classic-1.1.2.jar
+set CLASSPATH=%CLASSPATH%;lib\logback-core-1.1.2.jar
+set CLASSPATH=%CLASSPATH%;lib\slf4j-api-1.7.7.jar
+set CLASSPATH=%CLASSPATH%;%JAVA_HOME%\lib\tools.jar
+set CLASSPATH=%CLASSPATH%;%JAVA_HOME%\jre\lib\jfxrt.jar
+set CLASSPATH=%CLASSPATH%;target\jitwatch-1.0.0-SNAPSHOT.jar
+
+echo %CLASSPATH%
+
+"%JAVA_HOME%\bin\java" -Djava.library.path=%JAVA_HOME%\lib\amd64 -classpath "%CLASSPATH%" org.adoptopenjdk.jitwatch.jarscan.visualiser.HistoPlotter %1
+@REM ---------------------------------------------------------------
+
---
+++ histoPlotter.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+unamestr=`uname`
+if [ "$JAVA_HOME" = '' ]; then
+  if [ "$unamestr" = 'Darwin' ]; then
+     export JAVA_HOME=`/usr/libexec/java_home`
+  else
+     echo "JAVA_HOME has not been set."
+     exit 0;
+  fi
+fi
+
+# Requires the jar to be built using
+# mvn package
+# or
+# ant jar
+#
+#
+# You may need to set -Xmx (max heap) and -XX:MaxPermSize
+# if your hotspot.log references a lot of classes
+
+CLASSPATH=$CLASSPATH:lib/logback-classic-1.1.2.jar
+CLASSPATH=$CLASSPATH:lib/logback-core-1.1.2.jar
+CLASSPATH=$CLASSPATH:lib/slf4j-api-1.7.7.jar
+CLASSPATH=$CLASSPATH:$JAVA_HOME/lib/tools.jar
+CLASSPATH=$CLASSPATH:$JAVA_HOME/jre/lib/jfxrt.jar
+CLASSPATH=$CLASSPATH:target/jitwatch-1.0.0-SNAPSHOT.jar
+
+"$JAVA_HOME/bin/java" -Djava.library.path=$JAVA_HOME/lib/amd64 -cp "$CLASSPATH" org.adoptopenjdk.jitwatch.jarscan.visualiser.HistoPlotter $1
