df7a12584bb54a93197999bcca7bee6bb0d233a0
Cary Robbins <carymrobbins@gmail.com>
2017-07-28 10:05:00 -0500

    Updated launch scripts to support any working dir

--- launchUI.sh
+++ launchUI.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+# Get script dir, resolving symlinks; works on Linux and Mac.
+jitwatch_dir=$(python -c '
+from os.path import *
+from sys import *
+print(dirname(realpath(argv[1])))
+' "$0")
+
+CLASSPATH=$CLASSPATH:$JAVA_HOME/lib/tools.jar
+
 unamestr=`uname`
 if [ "$JAVA_HOME" = '' ]; then
   if [ "$unamestr" = 'Darwin' ]; then
@@ -19,13 +28,13 @@ fi
 # You may need to set -Xmx (max heap) and -XX:MaxPermSize
 # if your hotspot.log references a lot of classes
 
-CLASSPATH=$CLASSPATH:lib/logback-classic-1.1.2.jar
-CLASSPATH=$CLASSPATH:lib/logback-core-1.1.2.jar
-CLASSPATH=$CLASSPATH:lib/slf4j-api-1.7.7.jar
 CLASSPATH=$CLASSPATH:$JAVA_HOME/lib/tools.jar
 CLASSPATH=$CLASSPATH:$JAVA_HOME/jre/lib/jfxrt.jar
-CLASSPATH=$CLASSPATH:core/target/classes
-CLASSPATH=$CLASSPATH:ui/target/classes
-CLASSPATH=$CLASSPATH:ui/src/main/resources
+CLASSPATH=$CLASSPATH:$jitwatch_dir/lib/logback-classic-1.1.2.jar
+CLASSPATH=$CLASSPATH:$jitwatch_dir/lib/logback-core-1.1.2.jar
+CLASSPATH=$CLASSPATH:$jitwatch_dir/lib/slf4j-api-1.7.7.jar
+CLASSPATH=$CLASSPATH:$jitwatch_dir/core/target/classes
+CLASSPATH=$CLASSPATH:$jitwatch_dir/ui/target/classes
+CLASSPATH=$CLASSPATH:$jitwatch_dir/ui/src/main/resources
 
 "$JAVA_HOME/bin/java" -Djava.library.path=$JAVA_HOME/lib/amd64 -cp "$CLASSPATH" $@ org.adoptopenjdk.jitwatch.launch.LaunchUI
